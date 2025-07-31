db2ad3366bbb00585ccddc7dd1442fa913849421
N S R K Sree Charan <srkcharan@kitaab.biz>
2025-07-31 14:53:50 +0530

    removed app.listen to make it serverless to vercel deployement

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app=require("./app");
 const connectDB=require("./config/db");
 connectDB();
 
-const PORT=process.env.PORT || "5000";
-app.listen(PORT,()=>{
-    console.log("Server Running at PORT http://localhost:5000");
-});
+// const PORT=process.env.PORT || "5000";
+// app.listen(PORT,()=>{
+//     console.log("Server Running at PORT http://localhost:5000");
+// });
+
+module.exports = app;
