3816695b7c02c308494805aa0f0a2e500f1938ca
N S R K Sree Charan <srkcharan@kitaab.biz>
2025-08-11 23:27:00 +0530

    added dotenv

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose=require("mongoose");
+require("dotenv").config();
 
 const connectDB=async()=>{
     try{
+        console.log('MONGODB_URI:', process.env.MONGODB_URI);
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB Connected Successfully");
     }catch(err){
