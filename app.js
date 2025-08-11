0961b00a1f491267d1042b939fb8e1f6d5751503
nsrksreecharan <75178502+nsrksreecharan@users.noreply.github.com>
2025-08-11 22:09:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,26 @@ const {protect}=require("./middleware/authentication");
 
 const app=express();
 app.use(morgan("dev"));
-app.use(cors());
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://nsrksreecharan.github.io'
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
+
 app.use(express.json());
 app.use("/task-api",protect,tasksRoute);
 app.use("/user",userRoute);
