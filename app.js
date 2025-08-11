50a9e6a2a46d2544577c6bc42d9258bc84f37883
N S R K Sree Charan <srkcharan@kitaab.biz>
2025-08-11 23:16:47 +0530

    removed cors

--- app.js
+++ app.js
@@ -8,19 +8,7 @@ const {protect}=require("./middleware/authentication");
 
 const app=express();
 app.use(morgan("dev"));
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://nsrksreecharan.github.io",
-      "https://nsrksreecharan.github.io/Task-Management-App",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 app.use("/task-api",protect,tasksRoute);
 app.use("/user",userRoute);
