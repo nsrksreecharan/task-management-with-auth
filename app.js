5ea9d53cdf22b2a8157b8ff4fd8c2be8c938bdef
N S R K Sree Charan <srkcharan@kitaab.biz>
2025-08-11 22:46:31 +0530

    added origin resolved cors errors

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ const {protect}=require("./middleware/authentication");
 
 const app=express();
 app.use(morgan("dev"));
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/task-api",protect,tasksRoute);
 app.use("/user",userRoute);
